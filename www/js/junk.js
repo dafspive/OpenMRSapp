d6ccf987d0252143df1911fb9cbbcec34fc438d6
mwunger-soic <mwunger@comcast.net>
2015-06-23 15:07:01 -0400

    login working

---
+++ www/js/junk.js
@@ -0,0 +1,243 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var patData;
+var xhr;
+
+function Login()
+{
+    //check login
+    
+    //connect to openMRS
+    
+    //open page
+    location.href='contentPage.html';
+}
+
+function getPatInfo()
+{
+    var patUUID = 'ef42a5ee-1e0c-4870-adc1-3260af43becb';
+    //set get
+    var daturl = 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient/'+patUUID;
+    //get data from openmrs
+    
+    //getJSON(daturl).then(function(data) 
+    //{
+    //    alert('Your Json result is:  ' + data.result); //you can comment this, i used it to debug
+
+    //    result.innerText = data.result; //display the result in an HTML element
+    //}, function(status) { //error detection....
+    //    alert('Something went wrong.');
+    //});
+    
+    
+    getRequest(daturl);
+    //parse data
+    //document.getElementById("myDiv").innerHTML = xhr.responseText;
+    //alert('Patient Data: ' + patData);
+}
+
+function patientInfo(patUUID)
+{
+    // Initialize the Ajax request (We ignore IE5, IE6 here)
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient/'+patUUID);
+    xhr.setRequestHeader("Authorization", "Basic bWhlYWx0aDpNaGVhbHRoQDEyMw==");
+    // Track the state changes of the request
+    xhr.onreadystatechange = function () 
+            {
+                // Ready state 4 means the request is done
+                if (xhr.readyState === 4) 
+                {
+                    // 200 is a successful
+                    if (xhr.status === 200) 
+                    {
+                        document.getElementById("myDiv").innerHTML = xhr.responseText 
+                        // loading data into myDiv
+                    } else 
+                    {
+                        alert('Error: ' + xhr.status);
+                        // An error occurred during the request
+                    }
+                }
+            }
+}
+
+var getJSON = function(url) {
+    return new Promise(function(resolve, reject) 
+        {
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', url, true);
+            xhr.responseType = 'json';
+            xhr.onload = function() 
+                {
+                    var status = xhr.status;
+                    if (status == 200) 
+                    {
+                        resolve(xhr.response);
+                    } else {
+                        reject(status);
+                    }
+                };
+            xhr.send();
+        });
+    };
+
+
+
+
+
+
+function getRequest(daturl)
+{
+    //alert('start getrequest' + getFunction);
+    // Initialize the Ajax request (We ignore IE5, IE6 here)
+    xhr = new XMLHttpRequest();
+    xhr.open('get', daturl);
+    //xhr.responseType = 'json';
+    xhr.setRequestHeader("Authorization", "Basic bWhlYWx0aDpNaGVhbHRoQDEyMw==");
+    // Track the state changes of the request
+    xhr.onreadystatechange = function () 
+        {
+            // Ready state 4 means the request is done
+                if (xhr.readyState === 4) 
+                {
+                    // 200 is a successful
+                    if (xhr.status === 200) 
+                    {
+                        
+                        //alert('Godd data');
+                        document.getElementById("myDiv").innerHTML = xhr.responseText;
+                        //var myObj = eval ( xhr.responseText );
+                        //var parsedData = JSON.parse(myObj);
+
+                        //myFunction(myObj);
+                        //arr.display;
+                        //document.getElementById("disp").innerHTML = parsedData.display;
+                        
+                        
+                        //patData = JSON.parse(xhr.responseText);
+                        //resolve(xhr.response);
+
+                        //var jsonResponse = JSON.parse(xhr.responseText);
+                        
+                        
+                        //var strJSON = xhr.responseText;
+                        //var objJSON = eval("(function(){return " + strJSON + ";})()");
+                        //alert(objJSON.result);
+                        //alert(objJSON.person);
+                        //var myArr = JSON.parse(xhr.responseText);
+                        //myFunction(myArr);
+                    } else 
+                    {
+                        alert('Error: ' + xhr.status);
+                        // An error occurred during the request
+                    }
+                }
+        };
+    // Send the request to '/ws/rest/v1/patient'
+    xhr.send();
+}
+
+function myFunction(arr) 
+{
+    alert('start parse');
+    var out = "";
+    out = arr.display;
+    document.getElementById("disp").innerHTML = out;
+    }
+
+function doAjax(url, headers)
+{
+    xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.onreadystatechange = function () 
+    {
+        if (xhr.readyState === 4) 
+            {
+            // 200 is a successful
+            if (xhr.status === 200) 
+                {
+                    console.log(xhr.responseText);
+                    resolve(xhr.responseText);
+                } else {
+                    alert('Error: '+ xhr.status);
+                }
+            }
+    }
+    xhr.setRequestHeader("Authorization", localStorage['authKey']);
+    
+    xhr.send();
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function connOpenMRS()
+{
+    // Initialize the Ajax request (We ignore IE5, IE6 here)
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient?q=John');
+            // Track the state changes of the request
+            xhr.onreadystatechange = function () 
+            {
+                // Ready state 4 means the request is done
+                if (xhr.readyState === 4) 
+                {
+                    // 200 is a successful
+                    if (xhr.status === 200) 
+                    {
+                        document.getElementById("myDiv").innerHTML = xhr.responseText 
+                        // loading data into myDiv
+                    } else 
+                    {
+                        alert('Error: ' + xhr.status);
+                        // An error occurred during the request
+                    }
+                }
+            }
+            // Send the request to '/ws/rest/v1/patient'
+            xhr.send();
+}
+function checkForm() {
+if (localStorage["pin"] != 'undefined') {
+document.getElementById("loginForm").style.display = 'block';
+document.getElementById("pinForm").style.display = 'none';
+} else {
+document.getElementById("loginForm").style.display = 'none';
+document.getElementById("pinForm").style.display = 'block';
+}
+}
+function doLogin() {
+    var userLoggedIn = false;
+    username = document.getElementById("usernameField").value;
+    password = document.getElementById("passwordField").value;
+    window.btoa(username+':'+password);
+    localStorage['authKey']= 'Basic '+ window.btoa(username+':'+password);
+    console.log(username + ':' + password);
+    authresponse = doAjax('http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/session');
+    
+    
+    if(userLoggedIn) {
+        document.getElementById("loginForm").style.display = 'none';
+        document.getElementById("pinForm").style.display = 'block';
+    } else {
+    }
+}
+
+<script>
+            document.addEventListener('DOMContentLoaded', getPatInfo())
+        </script>
--- www/js/main.js
+++ www/js/main.js
@@ -3,176 +3,155 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var patData;
-var xhr;
 
-function Login()
+var ajaxResult;
+
+function checkForm()
 {
-    //check login
-    
-    //connect to openMRS
-    
-    //open page
-    location.href='contentPage.html';
+    console.log('Check Form');
+    //set login status
+    localStorage['loginStatus'] = false;
+    //check to see if pin stored
+    console.log(localStorage['pin']);
+    if (localStorage["pin"] == undefined) 
+    {
+        document.getElementById("loginForm").style.display = 'block';
+        document.getElementById("pwError").style.display = 'none';
+        document.getElementById("pinForm").style.display = 'none';
+        document.getElementById("pinError").style.display = 'none';
+    } else 
+    {
+        document.getElementById("loginForm").style.display = 'none';
+        document.getElementById("pwError").style.display = 'none';
+        document.getElementById("pinForm").style.display = 'block';
+        document.getElementById("pinError").style.display = 'none';
+    }
 }
 
-function getPatInfo()
-{
-    var patUUID = 'ef42a5ee-1e0c-4870-adc1-3260af43becb';
-    //set get
-    var daturl = 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient/'+patUUID;
-    //get data from openmrs
-    
-    //getJSON(daturl).then(function(data) 
-    //{
-    //    alert('Your Json result is:  ' + data.result); //you can comment this, i used it to debug
 
-    //    result.innerText = data.result; //display the result in an HTML element
-    //}, function(status) { //error detection....
-    //    alert('Something went wrong.');
-    //});
-    
-    
-    getRequest(daturl);
-    //parse data
-    //document.getElementById("myDiv").innerHTML = xhr.responseText;
-    //alert('Patient Data: ' + patData);
+//login functions
+function doPwLogin()
+{
+    //store data
+    username = document.getElementById("usernameField").value;
+    password = document.getElementById("passwordField").value;
+    localStorage['authKey'] = 'Basic ' + window.btoa(username + ':' + password);
+    console.log("Password entered");
+    //check auth
+    localStorage['loginStatus'] = "pw";
+    doAjax("http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/session");
 }
 
-function patientInfo(patUUID)
+function doPinLogin()
 {
-    // Initialize the Ajax request (We ignore IE5, IE6 here)
+    console.log("Pin Entered");
+    //check if pin is saved
+    if (localStorage["pin"] == undefined) 
+    {
+        //save pin
+        localStorage['pin'] = document.getElementById('pinField').value;
+        localStorage['loginStatus'] = "pin";
+        doAjax("http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/session");
+    } else
+    {
+        //check pin
+        if (localStorage['pin'] == document.getElementById('pinField').value) 
+        {
+            console.log("Pin good");
+            localStorage['loginStatus'] = "pin";
+            doAjax("http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/session");
+        }
+        else 
+        {
+            console.log("Pin bad");
+            document.getElementById("pinError").style.display = 'block';
+        }
+    }
+    
+    
+}
+
+//Ajax functions
+function doAjax(url) {
     var xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient/'+patUUID);
-    xhr.setRequestHeader("Authorization", "Basic bWhlYWx0aDpNaGVhbHRoQDEyMw==");
-    // Track the state changes of the request
-    xhr.onreadystatechange = function () 
+    xhr.open('get', url);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) 
             {
-                // Ready state 4 means the request is done
-                if (xhr.readyState === 4) 
-                {
-                    // 200 is a successful
-                    if (xhr.status === 200) 
-                    {
-                        document.getElementById("myDiv").innerHTML = xhr.responseText 
-                        // loading data into myDiv
-                    } else 
-                    {
-                        alert('Error: ' + xhr.status);
-                        // An error occurred during the request
-                    }
-                }
+                ajaxResult = JSON.parse(xhr.responseText);
+                console.log(xhr.responseText);
+                //check response;
+                ajaxResponse();
+            } else {
+                alert('Error: ' + xhr.status);
             }
-}
-
-var getJSON = function(url) {
-    return new Promise(function(resolve, reject) 
-        {
-            var xhr = new XMLHttpRequest();
-            xhr.open('get', url, true);
-            xhr.responseType = 'json';
-            xhr.onload = function() 
-                {
-                    var status = xhr.status;
-                    if (status == 200) 
-                    {
-                        resolve(xhr.response);
-                    } else {
-                        reject(status);
-                    }
-                };
-            xhr.send();
-        });
+        }
     };
-
-
-
-
-
-
-function getRequest(daturl)
-{
-    //alert('start getrequest' + getFunction);
-    // Initialize the Ajax request (We ignore IE5, IE6 here)
-    xhr = new XMLHttpRequest();
-    xhr.open('get', daturl);
-    //xhr.responseType = 'json';
-    xhr.setRequestHeader("Authorization", "Basic bWhlYWx0aDpNaGVhbHRoQDEyMw==");
-    // Track the state changes of the request
-    xhr.onreadystatechange = function () 
-        {
-            // Ready state 4 means the request is done
-                if (xhr.readyState === 4) 
-                {
-                    // 200 is a successful
-                    if (xhr.status === 200) 
-                    {
-                        
-                        //alert('Godd data');
-                        document.getElementById("myDiv").innerHTML = xhr.responseText;
-                        //var myObj = eval ( xhr.responseText );
-                        //var parsedData = JSON.parse(myObj);
-
-                        //myFunction(myObj);
-                        //arr.display;
-                        //document.getElementById("disp").innerHTML = parsedData.display;
-                        
-                        
-                        //patData = JSON.parse(xhr.responseText);
-                        //resolve(xhr.response);
-
-                        //var jsonResponse = JSON.parse(xhr.responseText);
-                        
-                        
-                        //var strJSON = xhr.responseText;
-                        //var objJSON = eval("(function(){return " + strJSON + ";})()");
-                        //alert(objJSON.result);
-                        //alert(objJSON.person);
-                        //var myArr = JSON.parse(xhr.responseText);
-                        //myFunction(myArr);
-                    } else 
-                    {
-                        alert('Error: ' + xhr.status);
-                        // An error occurred during the request
-                    }
-                }
-        };
-    // Send the request to '/ws/rest/v1/patient'
+    xhr.setRequestHeader("Authorization", localStorage['authKey']);
     xhr.send();
 }
 
-function myFunction(arr) 
+function ajaxResponse()
 {
-    alert('start parse');
-    var out = "";
-    out = arr.display;
-    document.getElementById("disp").innerHTML = out;
+    console.log("check response");
+    console.log("User authenticated? " +localStorage['authenticated']);
+    //check to see if authenicated
+    if (localStorage["authenticated"] == 'false' || localStorage["authenticated"]== undefined )
+    {
+        authenticatedFalse();
+    } 
+    else
+    {
+        authenticatedTrue();
     }
+}
 
-function connOpenMRS()
+function authenticatedFalse()
 {
-    // Initialize the Ajax request (We ignore IE5, IE6 here)
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', 'http://gw151.iu.xsede.org:8080/openmrs/ws/rest/v1/patient?q=John');
-            xhr.setRequestHeader("Authorization", "Basic bWhlYWx0aDpNaGVhbHRoQDEyMw==");
-            // Track the state changes of the request
-            xhr.onreadystatechange = function () 
+    console.log("check user response");
+        //check to see if resonse is for authentication
+        //if(ajaxResult.hasOwnProperty('user')) 
+        if (ajaxResult.authenticated == true)
+        {
+            //store authenticated
+            console.log(ajaxResult.user.display);
+            if (ajaxResult.authenticated == true) 
             {
-                // Ready state 4 means the request is done
-                if (xhr.readyState === 4) 
+                console.log("user authenticated");
+                localStorage['authenticated'] = true;
+                if (localStorage['loginStatus'] == "pw")
                 {
-                    // 200 is a successful
-                    if (xhr.status === 200) 
-                    {
-                        document.getElementById("myDiv").innerHTML = xhr.responseText 
-                        // loading data into myDiv
-                    } else 
-                    {
-                        alert('Error: ' + xhr.status);
-                        // An error occurred during the request
-                    }
+                    //turn on pin
+                    document.getElementById("loginForm").style.display = 'none';
+                    document.getElementById("pinForm").style.display = 'block';
                 }
+                
+            } else
+            {
+                console.log("Bad user name");
             }
-            // Send the request to '/ws/rest/v1/patient'
-            xhr.send();
+        }
+        else
+        {
+            console.log("Bad user name");
+            document.getElementById("pwError").style.display = 'block';
+        }
+}
+
+function authenticatedTrue()
+{
+    console.log("User logged in");
+        if (localStorage['loginStatus'] == "pw")
+        {
+            //turn on pin
+            document.getElementById("loginForm").style.display = 'none';
+            document.getElementById("pinForm").style.display = 'block';
+        }
+        else
+        {
+            //goto context page
+            console.log("goto context page");
+            location.href='contentPage.html';
+        }
 }
